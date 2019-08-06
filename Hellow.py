859763f285df79acd2eb311c83e6633251986f81
rubenlasso <ruben.lasso88@homail.com>
2019-08-05 21:49:55 -0500

    upload basic calc

---
+++ Hellow.py
@@ -0,0 +1,10 @@
+#thi is my first python program
+#Developer: Ruben Dario Lasso
+a=5
+b=10
+print  ("::: BASIC CAlC :::")
+
+print("the add: ", a+b)
+print("the subs is : ", a-b)
+print("the div is : ", a/b)
+print("the mult is : ", a*b)
